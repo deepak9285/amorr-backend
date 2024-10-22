be1b2f7a26c0c31a7b7bc681097c4128ef1c0efb
Kaushal <kaushallokhande12345@gamil.com>
2024-10-22 22:46:59 +0530

    updated chat api

--- src/db/db.js
+++ src/db/db.js
@@ -2,9 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(
-      `${process.env.DB}/${process.env.DB_NAME}`
-    );
+    const connectionInstance = await mongoose.connect(process.env.DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(
       `MongoDB connected! DB HOST : ${connectionInstance.connection.host}`
     );
