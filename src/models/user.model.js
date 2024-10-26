6d36bc55540db9fa6e1fdd2578c657c40e39d054
Kaushal <kaushallokhande12345@gamil.com>
2024-10-26 20:04:51 +0530

    chat

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -24,6 +24,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  isOnline: { 
+    type: Boolean,
+    default: false
+  },
   resetPasswordToken:String,
   resetPasswordExpires:Date,
   refreshToken: String,
--- src/routers/chat.routers.js
+++ src/routers/chat.routers.js
@@ -1,14 +1,24 @@
 import { Router } from "express";
 
 import {
+    createAGroupChat,
     createOrGetAOneOnOneChat,
-    getAllChats
+    getAllChats,
+    getUserStatus,
+    blockUser,
+    unblockUser,
+    checkBlockStatus
 } from "../controllers/chat.controllers.js";
 
 const router = Router();
 
 router.route("/oneOnone/:receiverId").post(createOrGetAOneOnOneChat);
+router.route("/group").post(createAGroupChat);
 router.route("/allchats").post(getAllChats);
+router.route("/status/:userId").get(getUserStatus);
 
+router.route("/block/:userToBlockId").post(blockUser);
+router.route("/unblock/:userToUnblockId").post(unblockUser);
+router.route("/checkblock/:userToCheckId").get(checkBlockStatus);
 export default router;
 
