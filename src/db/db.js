0d5fce1b9b5415fd330983a6da95d6141dcc12c3
AyushAwasthi2384 <ayushawasthicoding@gmail.com>
2024-10-29 16:25:31 +0530

    db err

--- src/db/db.js
+++ src/db/db.js
@@ -23,8 +23,8 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.DB}/${process.env.DB_NAME}`
-    );
+      `${process.env.DB}/${process.env.DB_NAME}` 
+    ); 
     console.log(
       `MongoDB connected! DB HOST : ${connectionInstance.connection.host}`
     );
